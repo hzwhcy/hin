396151b34d8a3b18dc701c2c0863e5aee196a95e
Hzw <1109017154@qq.com>
2022-11-25 21:21:34 +0800

    web框架

---
+++ hhin/router.go
@@ -0,0 +1,73 @@
+package hhin
+
+import (
+	"fmt"
+	"strings"
+)
+
+type router struct {
+	roots    map[string]*node
+	handlers map[string]HandlerFunc
+}
+
+func NewRouter() *router {
+	return &router{handlers: make(map[string]HandlerFunc), roots: make(map[string]*node)}
+}
+
+func (r *router) addRoute(method string, pattern string, handler HandlerFunc) {
+	parts := parsePattern(pattern)
+	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	r.handlers[key] = handler
+}
+
+func (r *router) handle(c *Context) {
+	key := c.Req.Method + "-" + c.Req.URL.Path
+	if handler, ok := r.handlers[key]; ok {
+		handler(c)
+	} else {
+		fmt.Fprintf(c.Writer, "404 NOT FOUND: %s\n", c.Req.URL)
+	}
+}
+
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+	parts := make([]string, 0)
+	for _, item := range vs {
+		if item != "" {
+			parts = append(parts, item)
+			if item[0] == '*' {
+				break
+			}
+		}
+	}
+	return parts
+}
+
+func (r *router) getRoute(method string, path string) (*node, map[string]string) {
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	n := root.search(searchParts, 0)
+
+	if n != nil {
+		parts := parsePattern(n.pattern)
+		for index, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[index]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[index:], "/")
+			}
+		}
+		return n, params
+	}
+	return nil, nil
+}
